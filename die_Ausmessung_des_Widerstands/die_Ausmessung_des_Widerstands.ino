f024c26f9ec47dc95199e78b9a8f068b73e0d6c9
Lie Yuan <yuanlie520@gmail.com>
2017-07-08 13:01:58 +0200

    smallprogram

---
+++ die_Ausmessung_des_Widerstands/die_Ausmessung_des_Widerstands.ino
@@ -0,0 +1,20 @@
+int Potpin = A4;
+float r = 220.0 ;
+float Value,R;
+
+void setup()   
+{                
+   Serial.begin(9600);
+ 
+}
+
+void loop() 
+{
+   Value = analogRead(Potpin);
+   R = (1023.0 - Value) / (Value/r);  
+   Serial.println( R );
+   delay( 1000 );
+}
+
+
+  
