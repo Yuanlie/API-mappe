351226a62d405df79a918df7801d12838adf68ee
Lie Yuan <yuanlie520@gmail.com>
2017-07-10 22:38:28 +0200

    The photoresistor control the LED

---
+++ a small program/The_photoresistor_controll_the_LED.ino
@@ -0,0 +1,29 @@
+int photocellPin = 8; 
+int photocellVal = 0; 
+int minLight = 200;   
+int ledPin = 4;
+int ledState = 0; 
+ 
+void setup() {
+  pinMode(ledPin, OUTPUT); 
+  Serial.begin(9600);
+}
+ 
+void loop() {
+  photocellVal = analogRead(photocellPin);
+  Serial.println(photocellVal);   
+   
+ 
+  if (photocellVal < minLight && ledState == 0) {
+    digitalWrite(ledPin, HIGH);
+    ledState = 1;
+  }
+   
+  
+  if (photocellVal > minLight && ledState == 1) {
+    digitalWrite(ledPin, LOW); 
+    ledState = 0;
+  }  
+   
+  delay(100);       
+}
