64a2566ace0dba04684cad752b4f36db7c3e1751
Lie Yuan <yuanlie520@gmail.com>
2017-07-11 15:57:29 +0200

    Benutzen von millis-funktion

---
+++ Anforderungen/The_photoresistor_control_the_LED_in_one_minute_.ino
@@ -0,0 +1,32 @@
+int photocellPin = A2; 
+int photocellVal = 0; 
+int minLight = 200;   
+int ledPin = 4;
+int ledState = 0; 
+ 
+void setup() {
+  pinMode(ledPin, OUTPUT); 
+  Serial.begin(9600);
+}
+ 
+void loop() {
+  if (millis()<60000)
+{
+  photocellVal = analogRead(photocellPin);
+  Serial.println(photocellVal);   
+   
+ 
+  if (photocellVal < minLight && ledState == 0) {
+    digitalWrite(ledPin, HIGH);
+    ledState = 1;
+  }
+   
+  
+  if (photocellVal > minLight && ledState == 1) {
+    digitalWrite(ledPin, LOW); 
+    ledState = 0;
+  }  
+   
+  delay(100);       
+}
+}
