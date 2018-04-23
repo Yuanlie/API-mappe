eaa44f1c7e82290bffda427a26f121ee5e5933f2
Lie Yuan <yuanlie520@gmail.com>
2018-04-23 21:43:59 +0200

    11

---
+++ Rechneruebung_1/Rechneruebung_1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream> //Präprozessoranweisungen.
+
+using namespace std;
+
+int main() //Funktionskopf
+{
+    cout << endl;
+    cout << "mein erstes Programm";
+    cout << endl;
+    return 0;
+    
+} // Funktionsende.
+
